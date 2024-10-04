f8714bbbf5954d17167cb1a7c00fc52ad964ecd1
Taylor Dean <taylor@makeshift.dev>
2024-10-04 19:36:04 -0400

    convert traceid / spanid to string

--- Sources/EmbraceCore/Capture/Network/URLSessionTaskHandler.swift
+++ Sources/EmbraceCore/Capture/Network/URLSessionTaskHandler.swift
@@ -204,8 +204,8 @@ final class DefaultURLSessionTaskHandler: URLSessionTaskHandler {
         }
 
         let value = W3C.traceparent(from: span.context)
-        task.injectHeader(withKey: "X-B3-TraceId", value: span.context.traceId)
-        task.injectHeader(withKey: "X-B3-SpanId", value: span.context.spanId)
+        task.injectHeader(withKey: "X-B3-TraceId", value: span.context.traceId.hexString)
+        task.injectHeader(withKey: "X-B3-SpanId", value: span.context.spanId.hexString)
         task.injectHeader(withKey: "X-B3-Sampled", value: "1")
         if task.injectHeader(withKey: W3C.traceparentHeaderName, value: value) {
             return value
